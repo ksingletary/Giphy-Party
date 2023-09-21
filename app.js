09a4a8172b65099fe7d6953c814ed62b05e42721
Keith Kodes <code.keith@Keiths-MBP.lan>
2023-09-21 12:12:26 -0400

    able to append gifs to page

--- app.js
+++ app.js
@@ -5,41 +5,32 @@ console.log("Let's get this party started!");
 // - Once the Giphy API has responded with data, append the GIF to the page
 // - Allow the user to search for as many GIFs as they would like and keep appending them to the page
 // - Allow the user to remove all of the GIFs by clicking a button
-const myBod = $('body');
+const gifcontainer = $('#container');
 const inpt = $('input');
 
-function searchGif () {
-    $("form").on("submit", async function(e) {
-        e.preventDefault();
 
-        let $inptVal = $('input').val();
-        $('input').val('');
-      
-        const res = await axios.get("http://api.giphy.com/v1/gifs/search", {
-          params: {
-            q: $inptVal,
-            api_key: "5aTZb37JarXQWEHZFuQV04LsfrSjMBEr",
-            limit: 100
-          }
-        });
-        appendImg(inpt.val());
-        console.log(res.data.data[0].images.original.url);
-      });    
-}
+$("form").on("submit", async function(e) {
+    e.preventDefault();
 
-
-async function appendImg(inpt){
+    let $inptVal = $('input').val();
+    inpt.val('');
+    
     const res = await axios.get("http://api.giphy.com/v1/gifs/search", {
         params: {
-        q: inpt,
+        q: $inptVal,
         api_key: "5aTZb37JarXQWEHZFuQV04LsfrSjMBEr",
         limit: 100
         }
     });
+    appendImg(res);
+    console.log(res.data.data[0].images.original.url);
+});    
+
+
+function appendImg(res){
     let $img = $('<img>');
     $img.attr('src', res.data.data[0].images.original.url);
-    myBod.append($img);
+    gifcontainer.append($img);
 }
 
-searchGif();
 
